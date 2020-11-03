44a3baf7608b531ba20e0127c95e4baf882bc123
Nix <nix3269@gmail.com>
2020-11-03 22:34:00 +0530

    fixed minor syntax bugs

--- app/models/license.js
+++ app/models/license.js
@@ -1,13 +1,7 @@
 let crypto = require('crypto');
 const keygen = require('../models/Keygen')
 class license {
-    // constructor(lic){
-    //     this.name= lic.name;
-    //     this.DOB = lic.DOB;
-    //     this.Address = lic.Address;
-    //     this.Expiry = lic.Expiry;
-    //     this.signature = lic.signature;
-    // }
+
     constructor(name,DOB,Address,Expiry,signature){
         this.name= name;
         this.DOB = DOB;
@@ -28,9 +22,6 @@ class license {
     }
 
     signTransaction(signingKey) {
-        // if (signingKey.getPublic('hex') !== this.fromAddress) {
-        //     throw new Error('You cannot sign transactions for other wallets!');
-        // }
         const hashTx = this.calculateHash();
         const sig = signingKey.sign(hashTx, 'base64');
 
--- app/routes/License.js
+++ app/routes/License.js
@@ -8,7 +8,6 @@ const keygen = require('../models/Keygen');
  * POST /book to save a new book.
  */
 function createlicense(req, res) {
-    let message;
     try {
         latest.findOne({ ide: 1 }, (err, block) => {
             let prev_hash1 = block.latesthash;
@@ -17,93 +16,99 @@ function createlicense(req, res) {
             var newBlock = new Block(obj);
             newBlock.save((err, block) => {
                 if (err) {
-                    message=err;
+                    message = err;
                 }
                 else { //If no errors, send it back to the client
                     latest.updateOne({ ide: 1 }, { latesthash: block.hash }, (err, op) => {
-                        if (err) { message={ message: "Latest hash could not be updated" }; }
-                        else { message={ message: "Block successfully added!", block }; }
+                        if (err) { res.send({ message: "Latest hash could not be updated" }); }
+                        else { res.send({ message: "Block successfully added!", block }); }
                     });
                 }
             });
         });
 
     } catch (e) {
-        message=e;
+        res.send(e);
     }
-    res.send(message);
 }
 
 function getBlock(req, res) {
     let message;
     if (!req.query.lichash) {
-        message = { message: "Licence Hash required" };
+        res.send({ message: "Licence Hash required" });
     }
-    else{
+    else {
         Block.findOne({ hash: req.query.lichash }, (err, block) => {
             if (err) {
-                message = err;
+                res.send(err);
             }
             else {
                 if (block == null || block == "") {
-                    message = { message: "License Not Found" };
+                    res.send({ message: "License Not Found" });
                     //If no errors, send it back to the client
                 } else {
                     try {
                         b = new Lic(block.License.name, block.License.DOB, block.License.Address, block.License.Expiry, "Placeholder");
                         b.signature = block.License.signature;
                         if (b.SignatureisValid) {
-                            message = b;
+                            res.send(b);
                         } else {
-                            message = { message: "Block has been compromised" };
+                            res.send({ message: "Block has been compromised" });
                         }
                     } catch (e) {
-                        message = e;
+                        res.send(e);
                     }
                 }
             }
         });
     }
-    res.send(message);
 }
 
 function updateBlock(req, res) {
-    let message;
-    if (!req.query.lichash) message={ message: "Licence Hash required" };
-    Block.findOne({ hash: req.query.lichash }, (err, block) => {
-        if (err) {
-            message=err;
-        }
-        else {
-            if (block == null || block == "") { message={ message: "License Not found" }; }
-             else {
-                try {
-                    newblock = new Lic(block.License.name, block.License.DOB, block.License.Address, block.License.Expiry, "Placeholder");
-                    b.signature = block.License.signature;
-                    if (req.params.name) {
-                        newblock.name = req.params.name;
+    if (!req.query.lichash) {
+        res.send({ message: "Licence Hash required" });
+    }
+    else {
+        Block.findOne({ hash: req.query.lichash }, (err, block) => {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                if (block == null || block == "") {
+                    res.send({ message: "License Not found" });
+                }
+                else {
+                    try {
+                        let b = new Lic(block.License.name, block.License.DOB, block.License.Address, block.License.Expiry, "Placeholder");
+                        b.signature = block.License.signature;
+                        if (req.query.name) {
+                            b.name = req.query.name;
+                        }
+                        if (req.query.DOB) {
+                            b.DOB = req.query.DOB;
+                        }
+                        if (req.query.Address) {
+                            b.Address = req.query.Address;
+                        }
+                        if (req.query.Expiry) {
+                            b.Expiry = req.query.Expiry;
+                        }
+                        b.signature = b.signTransaction(keygen.genFromPri(req.query.privkey));
+                        Block.updateOne(block, { License: b }, (err, block2) => {
+                            if (err) {
+                                res.send(err);
+                            }
+                            else {
+                                res.send(block2);
+                            }
+                        });
+                    } catch (e) {
+                        res.send(e);
                     }
-                    if (req.params.DOB) {
-                        newblock.DOB = req.params.DOB;
-                    }
-                    if (req.params.Address) {
-                        newblock.Address = req.params.Address;
-                    }
-                    if (req.params.Expiry) {
-                        newblock.Expiry = req.params.Expiry;
-                    }
-                    newblock.signature = newblock.signTransaction(keygen.genFromPri(req.query.prikey));
-                    Block.updateOne({ hash: req.params.lichash }, { License: newblock }, (err, block) => {
-                        if (err) message=err;
-                        message=block;
-                    })
-                } catch (e) {
-                    message=e;
                 }
             }
-        }
-    });
-    res.send(message);
+        });
+    }
 }
 function blockisValid(block) {
     newblock = new Lic(block.License.name, block.License.DOB, block.License.Address, block.License.Expiry, "Placeholder");
